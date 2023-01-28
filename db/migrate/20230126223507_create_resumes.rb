18fad5c7e678852cf425510ba20277021343ef1f
Daniel Dominguez <danielfromarg@gmail.com>
2023-01-28 13:57:11 -0300

    fixx

--- db/migrate/20230126223507_create_resumes.rb
+++ db/migrate/20230126223507_create_resumes.rb
@@ -10,6 +10,11 @@ class CreateResumes < ActiveRecord::Migration[7.0]
       t.text :ia_experience
       t.text :ia_skills
 
+      t.text :final_description
+      t.text :final_experience
+      t.text :final_skills
+      t.text :contact_email
+
       t.timestamps
     end
   end
